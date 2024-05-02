8e3d850a2b73a218f003a8896118f89bd29a59c3
AAteddy <tedsaasfaha@gmail.com>
2024-05-02 15:55:14 -0700

    0-add.py removed uneccessory spaces

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""
-Write a type-annotated function add that
+"""Write a type-annotated function add that
 takes a float a and a float b as arguments
 and returns their sum as a float.
 """
@@ -9,4 +8,3 @@ and returns their sum as a float.
 def add(a: float, b: float) -> float:
     """Add and return two floats"""
     return a + b
-
