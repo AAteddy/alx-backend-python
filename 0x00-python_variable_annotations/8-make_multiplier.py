d24e4e4414fd291705f5370538c0bbad265d0b11
Tedros Alem <tedsaasfaha@gmail.com>
2024-05-02 22:55:03 +0300

    [Complex types - functions] project-9 file (Callable) implemented

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function make_multiplier that takes a 
+float multiplier as argument and returns a function 
+that multiplies a float by multiplier.
+"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns a function that multiplies a float by multiplier"""
+
+    def multiply_float(num: float) -> float:
+        """A Callable function"""
+        return num * multiplier
+
+    return multiply_float
