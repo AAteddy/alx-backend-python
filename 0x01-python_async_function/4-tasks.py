90c343876da4e7103e79a9864800c2802865504d
Tedros Alem <tedsaasfaha@gmail.com>
2024-05-06 18:50:13 +0300

    [Fixed - removed white space] project file 3-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""Take the code from wait_n and alter it into 
-a new function task_wait_n. The code is nearly 
-identical to wait_n except task_wait_random 
+"""Take the code from wait_n and alter it into
+a new function task_wait_n. The code is nearly
+identical to wait_n except task_wait_random
 is being called.
 """
 
@@ -12,7 +12,7 @@ task_wait_random = __import__("3-tasks").task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """doc"""
+    """a func that calls task_wait_random"""
     delays_list = []
     for _ in range(n):
         delays_list.append(task_wait_random(max_delay))
